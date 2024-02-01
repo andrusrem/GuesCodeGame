92a3d590fb735756b329ee8529df2fd3e42e765a
andrusrem <andrusremets@gmail.com>
2024-02-01 21:20:35 +0200

    More quizz added
    Co-authored-by: Retro <dankmolot@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <script setup>
-  import { ref } from 'vue';
+  import { ref, reactive } from 'vue';
   import Quizz from './components/quizz.vue';
 
-  let playing = ref(false)
+  let state = reactive({
+    page: 'home',
+    points: 0,
+  })
+
   let currentQuestionCount = 0
   let currentQuestion = ref(null)
   let questions = [
-    { image: "quizzImages/quizz1.png", variants: ["Loop", "Function", "Array"], answer: "Loop" }
+    { image: "quizzImages/quizz1.png", variants: ["Loop", "Function", "Array"], answer: "Loop" },
+    { image: "quizzImages/quizz2.png", variants: ["Function", "Loop", "Array"], answer: "Array" },
+    { image: "quizzImages/guizz3.png", variants: ["Interface", "Class", "Enumirate"], answer: "Class" },
+    { image: "quizzImages/quizz4.png", variants: ["Enumirate", "Interface", "Class" ], answer: "Enumirate" },
+    { image: "quizzImages/quizz5.png", variants: [ "Class", "Interface", "Enumirate"], answer: "Interface" },
+    { image: "quizzImages/quizz6.png", variants: [ "Test Class", "Test Function", "Mingi jama"], answer: "Test Function" },
+    { image: "quizzImages/quizz7.png", variants: [ "HTML", "CSS", "Javascript"], answer: "Javascript" },
+    { image: "quizzImages/quizz8.png", variants: [ "C++", "C#", "Java"], answer: "C#" },
+    { image: "quizzImages/quizz9.png", variants: [ "XML", "HTML", "Juri Trei teab"], answer: "HTML" },
+    { image: "quizzImages/quizz10.png", variants: [ "SCSS", "CSS", "Ei tea"], answer: "CSS" },
+    
+
   ]
 
   // Randomize questions
@@ -16,20 +31,55 @@
     .map(({ value }) => value)
 
   function startPlaying() {
-    playing.value = true
+    if (currentQuestionCount >= questions.length) {
+      return finish()
+    }
+
+    state.page = 'quizz'
     currentQuestion.value = questions[currentQuestionCount]
+  }
+
+  function restartGame() {
+    currentQuestionCount = 0
+    state.points = 0
+    startPlaying()
+  }
+
+  function finish() {
+    state.page = 'result'
+  }
+
+  function nextPage() {
+    currentQuestionCount += 1
+    startPlaying()
+  }
+
+  function finishMessage() {
+    if (state.points == 0) {
+      return 'Sorry, oled loll!'
+    }
+    return 'Palju õnne! Nüüd oled andekas pois/tüdruk!'
   }
 </script>
 
 <template>
   <div class="dark:bg-gray-900 dark:text-gray-200 h-screen text-4xl">
-    <div v-if="!playing" class="container h-full flex justify-center items-center flex-col max-w-xl m-auto">
+    <div v-if="state.page == 'home'" class="container h-full flex justify-center items-center flex-col max-w-xl m-auto">
       <h1 class="text-3xl font-bold underline mb-20 text-6xl">
         Mis see on?
       </h1>
     <button class="bg-blue-700 hover:bg-blue-800 active:bg-blue-900 max-w-fit py-2 px-8 text-white rounded" @click="startPlaying">Mängi</button>
     </div>
-    <Quizz v-if="playing" :question="currentQuestion" />
+    <Quizz v-if="state.page == 'quizz'" :question="currentQuestion" :state="state" @finish="finish" @nextPage="nextPage" />
+    <div v-if="state.page == 'result'" class="flex items-center justify-center h-full flex-col">
+      <h1 class="text-3xl font-bold underline mb-20 text-6xl">
+        Mis see on?
+      </h1>
+      <p class="mb-5">{{  finishMessage() }}</p>
+      <p class="mb-5">Punktid: {{ state.points }}/{{  questions.length }}</p>
+      <button class="bg-blue-700 hover:bg-blue-800 active:bg-blue-900 max-w-fit py-2 px-8 text-white rounded" @click="restartGame">Mängi veel kord</button>
+
+    </div>
   </div>
 </template>
 
--- src/components/quizz.vue
+++ src/components/quizz.vue
@@ -1,15 +1,74 @@
 <script setup>
-    defineProps({
+    import { ref } from 'vue';
+
+    const emit = defineEmits('finish', 'nextPage')
+    let {state, question} = defineProps({
         question: Object,
+        state: Object,
     })
+
+    let currentAnswer = ref(null)
+
+    let interval
+    let time = ref(30)
+    function startTimer() {
+        clearInterval(interval)
+        interval = setInterval(() => {
+            time.value--
+            if(time.value == 0) {
+                clearInterval(interval)
+                result(question, null)
+            }
+        }, 1000)
+    }
+
+    function reset() {
+        currentAnswer.value = null
+        time.value = 30
+        startTimer()
+    }
+
+    reset()
+
+    function startDrag(event, item) {
+        event.dataTransfer.dropEffect = 'move'
+        event.dataTransfer.effectAllowed = 'move'
+        event.dataTransfer.setData('answer', item)
+    }
+
+    function onDrop(event, question) {
+        let answer = event.dataTransfer.getData('answer')
+        currentAnswer.value = answer
+        result(question, answer)
+    }
+    function result(question, answer) {
+        if(answer == question.answer) {
+            state.points += 1
+        }
+
+        setTimeout(() => {
+            reset()
+            emit('nextPage')
+        }, 1000);
+    }
 </script>
 
 <template>
     <div class="flex justify-center items-center flex-col container mx-auto py-20">
         <h1 class="text-6xl mb-8">Mis see on?</h1>
-        <img class="w-1/2 mb-8" :src="question.image">
+        <img class="w-1/2 min-w-[640px] mb-8" :src="question.image">
+        <div 
+            class="flex w-full items-center justify-center mb-20" @drop="onDrop($event, question)"
+            @dragover.prevent
+            @dragenter.prevent
+        >
+            <p class="mr-10">Aeg : {{time}}</p>
+            <p v-if="!currentAnswer" class="text-gray-300">Answer</p>
+            <p v-else :class="currentAnswer == question.answer ? 'text-green-600' : 'text-red-600'" >{{currentAnswer}}</p>
+            <p class="ml-10">Punktid : {{state.points}}</p>
+        </div>
         <div class="flex w-full justify-evenly">
-            <div v-for="variant in question.variants">
+            <div v-for="variant in question.variants" draggable="true" @dragstart="startDrag($event, variant)">
                 {{variant}}
             </div>
         </div>
